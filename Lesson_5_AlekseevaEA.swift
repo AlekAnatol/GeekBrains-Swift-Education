f15a940833b577ebf6e6c0bcf2ad337ed79e6467
Alekseeva EA <89096346135a@gmail.com>
2021-12-07 21:02:38 +0300

    Выполнено ДЗ к уроку 5

---
+++ Lesson_5_AlekseevaEA.swift
@@ -0,0 +1,391 @@
+//
+//  main.swift
+//  Lesson_5_AlekseevaEA
+//
+//  Created by Екатерина Алексеева on 07.12.2021.
+//
+
+import Foundation
+
+//MARK: - enums for CarProtocol
+enum Color: String {
+    
+    case red
+    case green
+    case blue
+    case black
+    case white
+    
+    var color: String {
+        
+        switch self {
+            
+        case .red:
+            return("красного цвета")
+        case .green:
+            return("зеленого цвета")
+        case .blue:
+            return("синего цвета")
+        case .black:
+            return("черного цвета")
+        case .white:
+            return("белого цвета")
+            
+        }
+    }
+}
+
+enum WindowsStatus {
+    case opened
+    case closed
+}
+
+enum EngineStatus {
+    case on
+    case off
+}
+
+//MARK: - enum for class SportCar
+enum DrivingMode: String{
+    case normal
+    case eco
+    case sport
+    
+    var mode: String{
+        switch self {
+        case .normal:
+            return ("стандартный режим управления")
+        case .eco:
+            return("экологичный режим управления")
+        case .sport:
+            return("спортивный режим управления")
+        }
+    }
+}
+
+//MARK: - enum for class TruckCar
+enum TrailerAttached {
+    case attached
+    case unattached
+}
+
+//MARK: - protocol Car
+protocol CarProtocol {
+    
+    var carModel: String { get set }
+    var carYearOfCreating: Int { get set }
+    var carColor: Color { get set }
+    var windowsStatus: WindowsStatus { get set }
+    var engineStatus: EngineStatus { get set }
+    
+    func checkEngineStatus()
+    func checkWindowsStatus()
+    mutating func changeCarColor (to color: Color)
+    mutating func startStopEngine(to status: EngineStatus)
+    mutating func openCloseWindow(to status: WindowsStatus)
+}
+
+//MARK: - extension for CarProtocol
+extension CarProtocol {
+    
+    func checkEngineStatus() {
+        engineStatus == .on ? print("Мотор заведен") : print("Мотор заглушен")
+    }
+    
+    func checkWindowsStatus() {
+        windowsStatus == .opened ? print("Окна открыты") : print("Окна закрыты")
+    }
+    
+    mutating func changeCarColor (to color: Color) {
+        
+        if color == carColor {
+            print("Машина уже \(carColor.color)")
+            
+        } else {
+            carColor = color
+            print("Машина перекрашена, теперь она \(carColor.color)")
+        }
+    }
+    
+    mutating func startStopEngine(to status: EngineStatus) {
+        
+        if status == EngineStatus.on, engineStatus == .off {
+            engineStatus = .on
+            print("Мотор заведен, поехали!")
+        }
+        
+        else if status == EngineStatus.on, engineStatus == .on {
+            print("Машина уже заведена, осталось определиться с местом прибытия")
+            
+        }
+        
+        else if status == EngineStatus.off, engineStatus == .on {
+            engineStatus = .off
+            print("Приехали! Мотор заглушен")
+            
+        } else {
+            print("Машина еще не заведена")
+            
+        }
+    }
+    
+    mutating func openCloseWindow(to status: WindowsStatus){
+        
+        if status == WindowsStatus.opened, windowsStatus == .closed {
+            windowsStatus = .opened
+            print("Окна открыты")
+        }
+        
+        else if status == WindowsStatus.opened, windowsStatus == .opened {
+            print("Окна уже открыты. Осторожно, может продуть")
+            
+        }
+        
+        else if status == WindowsStatus.closed, windowsStatus == .opened {
+            engineStatus = .off
+            print("Окна закрыты")
+            
+        } else {
+            print("Окна уже были закрыты")
+            
+        }
+    }
+}
+
+//MARK: - class SportCar
+final class SportCar: CarProtocol {
+    
+    internal var carModel: String
+    internal var carYearOfCreating: Int
+    internal var carColor: Color
+    internal var windowsStatus: WindowsStatus
+    internal var engineStatus: EngineStatus
+    
+    private var drivingMode: DrivingMode
+    
+    init(carModel: String,
+         carYearOfCreating: Int,
+         carColor: Color) {
+        
+        self.carModel = carModel
+        self.carYearOfCreating = carYearOfCreating
+        self.carColor = carColor
+        self.windowsStatus = .closed
+        self.engineStatus = .off
+        self.drivingMode = .normal
+        
+        print("Вы выбрали \(carModel) \(carYearOfCreating) года выпуска \(carColor.color) ")
+        print("Режим управления: включен \(drivingMode.mode)")
+    }
+    
+    
+    func openCloseWindow(to status: WindowsStatus) {
+        
+        if drivingMode == DrivingMode.sport {
+            
+            if status == .opened, windowsStatus == .closed {
+                print("В данном режиме управления не рекомендуется открывать окна")
+            }
+            
+            else if status == .opened, windowsStatus == .opened {
+                
+                print("В данном режиме управления опасно ездитьс открытыми окнами, они будут автоматически закрыты")
+                windowsStatus = .closed
+            }
+        }
+        
+        if drivingMode == DrivingMode.eco || drivingMode == DrivingMode.normal {
+            
+            if status == .opened, windowsStatus == .closed {
+                
+                print("Окна открыты, можно насладиться видами")
+                windowsStatus = .opened
+            }
+            else if status == .opened, windowsStatus == .opened {
+                
+                print("Окна уже открыты")
+            }
+        }
+        
+    }
+    
+    func changeDrivingMode(to mode: DrivingMode){
+        
+        if mode == .normal, drivingMode == .normal {
+            
+            print("Уже включен нормальный режим, выберите другой режим вождения или продоложайте движение")
+        }
+        
+        else if mode == .normal, drivingMode != .normal {
+            
+            print("Включен нормальный режим управления")
+            drivingMode = .normal
+        }
+        
+        else if mode == .eco, drivingMode == .eco {
+            
+            print("Уже включен экологичный режим, выберите другой режим вождения или продоложайте движение")
+        }
+        
+        else if mode == .eco, drivingMode != .eco {
+            
+            print("Включен экологичный режим управления")
+            drivingMode = .eco
+        }
+        
+        else if mode == .sport, drivingMode == .sport {
+            
+            print("Уже включен спортивный режим, выберите другой режим вождения или продоложайте движение")
+        }
+        
+        else if mode == .sport, drivingMode != .sport {
+            
+            print("Включен спортивный режим управления")
+            
+            if windowsStatus == .opened {
+                
+                drivingMode = .sport
+                windowsStatus = .closed
+                print("В целях безопасности окна закрыты")
+            }
+            
+            else {
+                drivingMode = .sport
+            }
+        }
+    }
+}
+
+extension SportCar: CustomStringConvertible {
+    var description: String {
+            return String("Это спортивный автомобиль марки \(carModel) \n")
+    }
+}
+
+//MARK: - class TruckCar
+final class TruckCar: CarProtocol {
+    
+    internal var carModel: String
+    internal var carYearOfCreating: Int
+    internal var carColor: Color
+    internal var windowsStatus: WindowsStatus
+    internal var engineStatus: EngineStatus
+    
+    private var isTrailerAttached: TrailerAttached
+    private let trailerVolume: Int
+    private var cargoVolumeInTrailer: Int
+    private var freeVolume: Int
+    
+    init(
+        carModel: String,
+        carYearOfCreating: Int,
+        carColor: Color,
+        trailerVolume: Int) {
+            self.carModel = carModel
+            self.carYearOfCreating = carYearOfCreating
+            self.carColor = carColor
+            self.windowsStatus = .closed
+            self.engineStatus = .off
+            
+            self.isTrailerAttached = .attached
+            self.trailerVolume = trailerVolume
+            self.cargoVolumeInTrailer = 0
+            self.freeVolume = trailerVolume
+            
+            print("Вы выбрали \(carModel) \(carYearOfCreating) года выпуска \(carColor.color) ")
+            print("У вашего автомобиля есть прицеп объемом \(trailerVolume) литров")
+        }
+    
+    func checkFreeVolume() {
+        isTrailerAttached == .attached ? print("В прицепе осталось \(freeVolume) литров") : print("Прицепа нет")
+    }
+    
+    func changeTrailerStatus(to status: TrailerAttached) {
+        
+        if status == .attached, isTrailerAttached == .attached {
+            print("Прицеп уже есть, второй присодединить невозможно")
+        }
+        
+        else if status == .attached, isTrailerAttached == .unattached {
+            isTrailerAttached = .attached
+            print("Прицеп присоединен, его объем \(trailerVolume) литров")
+            print("Свободного мест \(freeVolume) литров")
+        }
+        
+        else if status == .unattached, isTrailerAttached == .attached {
+            isTrailerAttached = .unattached
+            print("Прицеп отсоединен")
+        }
+        
+        else {
+            print("Прицепа уже нет")
+        }
+    }
+    
+    func loadCargo(volumeOfCargo: Int) {
+        
+        if isTrailerAttached == .unattached{
+            print("Прицепа нет, зугрузка невозможна")
+        }
+        
+        else {
+            if volumeOfCargo > freeVolume {
+                print("Нет столько свободного места")
+            }
+            
+            else {
+                cargoVolumeInTrailer += volumeOfCargo
+                freeVolume -= volumeOfCargo
+                print("Успешно загружено \(volumeOfCargo) литров груза. \nОсталось \(freeVolume) литров свободного места")
+            }
+        }
+    }
+    
+    func unloadCargo(volumeOfCargo: Int) {
+        
+        if isTrailerAttached == .unattached{
+            print("Прицепа нет, выгрузка невозможна")
+        }
+        
+        else {
+            if volumeOfCargo > cargoVolumeInTrailer {
+                print("Нет столько груза")
+            }
+            
+            else {
+                cargoVolumeInTrailer -= volumeOfCargo
+                freeVolume += volumeOfCargo
+                print("Успешно выгружено \(volumeOfCargo) литров груза. \nОсталось \(freeVolume) литров свободного места")
+            }
+        }
+    }
+}
+
+extension TruckCar: CustomStringConvertible {
+    var description: String {
+            return String("Это грузовой автомобиль марки \(carModel) \n")
+    }
+}
+
+var sportCar = SportCar(carModel: "Lambo", carYearOfCreating: 2020, carColor: .white)
+sportCar.openCloseWindow(to: .opened)
+sportCar.checkWindowsStatus()
+sportCar.changeDrivingMode(to: .sport)
+sportCar.checkWindowsStatus()
+sportCar.changeDrivingMode(to: .eco)
+sportCar.changeDrivingMode(to: .eco)
+sportCar.changeCarColor(to: .red)
+sportCar.checkEngineStatus()
+sportCar.startStopEngine(to: .on)
+sportCar.checkEngineStatus()
+print(sportCar)
+
+var truckCar = TruckCar(carModel: "Kamaz", carYearOfCreating: 2019, carColor: .green, trailerVolume: 15000)
+truckCar.changeTrailerStatus(to: .unattached)
+truckCar.changeTrailerStatus(to: .attached)
+truckCar.checkFreeVolume()
+truckCar.changeTrailerStatus(to: .unattached)
+truckCar.loadCargo(volumeOfCargo: 14000)
+truckCar.unloadCargo(volumeOfCargo: 15000)
+truckCar.checkFreeVolume()
+print(truckCar)
